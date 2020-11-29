5907ccd86de4df541de00f1fbd3f602156d05d2f
riccrusso2 <riccardo.russo@liceobanzi.edu.it>
2020-11-29 12:16:42 +0100

    finished contract for all the domain entities

--- src/transports/domain_entities/Route.java
+++ src/transports/domain_entities/Route.java
@@ -5,22 +5,23 @@ import java.util.List;
 
 /**
  * This ADT represents a route formed by a list of cities,
- * that a truck could go across
- */
+         * that a truck could go across
+         */
 public class Route {
     private List<City> cities;
     private Duration duration;
 
     /**
+     *
      * Instances a new route composed by the cities given
      * and that has the mileage time specified
      * @param cities The list of cities that form the route
      * @param gg Integer number of days of the mileage time
      * @param hh Integer number of hours of the mileage time
      * @param mm Integer number of minutes of the mileage time
+     * @throws NullPointerException if at least one of the parameters is null
      */
-    public Route(List<City> cities, int gg, int hh, int mm){
-
+    public Route(List<City> cities, int gg, int hh, int mm) throws NullPointerException {
     }
 
     public List<City> getCities() {
